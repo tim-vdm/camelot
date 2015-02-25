0a1761d777bbe877eed131cac2d0580ea3b5953e
Pieter-Jan Delaruelle <pieter-jan.delaruelle@vortex-financials.be>
2015-02-25 11:57:01 +0100

    Added fab-command to run tests with coverage

--- src/fabfile.py
+++ src/fabfile.py
@@ -5,3 +5,7 @@ def run_tests(tests='',with_debugger=None):
     if with_debugger is not None:
         os.environ['DEBUGGER'] = with_debugger
     api.local("python -m nose.core -v -s -x vfinance_ws/{}".format(tests))
+
+def run_tests_with_coverage(tests=''):
+    api.local("rm -rf cover")
+    api.local("export MALLOC_CHECK_=0 && python -m nose.core --ignore-files=.*.pyc --with-coverage --cover-html --cover-html-dir=cover --cover-package=vfinance_ws -v -s --with-xunit vfinance_ws/{}".format(tests))
