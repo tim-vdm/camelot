dfe220294e4328804997d109a7ed292c68cc3d10
Stephane Wirtel <stephane@wirtel.be>
2015-02-26 17:56:15 +0100

    [FIX] Disable the documentation of Python

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -268,7 +268,7 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+# intersphinx_mapping = {'http://docs.python.org/': None}
 
 
 html_theme = "sphinx_rtd_theme"
