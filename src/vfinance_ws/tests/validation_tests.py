94b88bc9f64304f8a10920e11812fb0dbd19aa0d
Stephane Wirtel <stephane@wirtel.be>
2015-03-04 23:20:19 +0100

    [REF] Clean the code and improve the validation of the messages

--- src/vfinance_ws/tests/validation_tests.py
+++ src/vfinance_ws/tests/validation_tests.py
@@ -1,9 +1,19 @@
 import unittest
+import os
 from decimal import Decimal
+import json
 
 import voluptuous
 
 from ..ws import validation_message
+
+
+DEMO_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'demo')
+
+
+def load_demo_json(fname):
+    with open(os.path.join(DEMO_DIR, "%s.json" % (fname,))) as infile:
+        return json.load(infile)
 
 
 def date_new(year, month, day):
@@ -12,6 +22,27 @@ def date_new(year, month, day):
         'month': month,
         'day': day
     }
+
+class ValidationTestCase(unittest.TestCase):
+    def test_01(self):
+        schema = {
+            'date': voluptuous.All(validation_message.DATE_SCHEMA, validation_message.ValidateDate)
+        }
+        document = {
+            'date': {
+                'month': 2,
+                'year': 2015,
+                'day': 29
+            }
+        }
+        doc, errors = validation_message.validate_document(document, schema)
+
+        self.assertEqual(errors, {
+            'date/day': {
+                'message': 'day is out of range for month', 
+                'value': 29
+            }
+        })
 
 
 class DocumentValidationTestCase(unittest.TestCase):
@@ -23,14 +54,7 @@ class DocumentValidationTestCase(unittest.TestCase):
 
         document = date_new(2015, 3, 1)
         doc = validator(document)
-        self.assertIn('month', doc)
-        self.assertEqual(doc['month'], 3)
-
-        self.assertIn('year', doc)
-        self.assertEqual(doc['year'], 2015)
-
-        self.assertIn('day', doc)
-        self.assertEqual(doc['day'], 1)
+        self.assertEqual(doc, {'day': 1, 'month': 3, 'year': 2015})
 
     def test_011_schema_date_wrong(self):
         validator = voluptuous.Schema(validation_message.DATE_SCHEMA)
@@ -39,20 +63,34 @@ class DocumentValidationTestCase(unittest.TestCase):
             document = date_new(2015, 13, -1)
             validator(document)
 
-    def test_012_schema_sex(self):
+    def test_012_validation_date(self):
+        schema = {
+            'date': voluptuous.All(validation_message.DATE_SCHEMA, validation_message.ValidateDate)
+        }
+
+        validator = voluptuous.Schema(schema)
+        with self.assertRaises(voluptuous.MultipleInvalid) as ex:
+            validator({'date': {'month': 2, 'year': 2015, 'day': 29}})
+        self.assertEqual(ex.exception.msg, 'day is out of range for month')
+
+
+    def test_013_schema_sex(self):
         schema = {
             voluptuous.Required('sex'): voluptuous.All(voluptuous.Upper, voluptuous.In(['M', 'F']))
         }
 
         validator = voluptuous.Schema(schema)
         doc = validator({'sex': 'M'})
-        self.assertEqual(doc['sex'], 'M')
+        self.assertEqual(doc, {'sex': 'M'})
+
         doc = validator({'sex': 'm'})
-        self.assertEqual(doc['sex'], 'M')
+        self.assertEqual(doc, {'sex': 'M'})
+
         doc = validator({'sex': 'F'})
-        self.assertEqual(doc['sex'], 'F')
+        self.assertEqual(doc, {'sex': 'F'})
+
         doc = validator({'sex': 'f'})
-        self.assertEqual(doc['sex'], 'F')
+        self.assertEqual(doc, {'sex': 'F'})
 
         with self.assertRaises(voluptuous.MultipleInvalid) as ex:
             validator({})
@@ -62,7 +100,7 @@ class DocumentValidationTestCase(unittest.TestCase):
             validator({'sex': 'I'})
         self.assertIsInstance(ex.exception.errors[0], voluptuous.InInvalid)
 
-    def test_013_schema_decimal(self):
+    def test_014_schema_decimal(self):
         schema = {
             voluptuous.Required('amount'): voluptuous.Coerce(Decimal)
         }
@@ -76,7 +114,7 @@ class DocumentValidationTestCase(unittest.TestCase):
         document = validator({'amount': "10.5"})
         self.assertIsInstance(document['amount'], Decimal)
 
-    def test_allow_none(self):
+    def test_015_allow_none(self):
         schema = {
             voluptuous.Required('product_id') : voluptuous.Any(None, voluptuous.Coerce(long))
         }
@@ -92,7 +130,7 @@ class DocumentValidationTestCase(unittest.TestCase):
         doc = validator({'product_id': "10"})
         self.assertEqual(doc, {'product_id': 10})
 
-    def test_not_none_string(self):
+    def test_016_not_none_string(self):
         schema = {
             'name': voluptuous.All(basestring, voluptuous.Length(min=2, max=10)),
         }
@@ -106,7 +144,7 @@ class DocumentValidationTestCase(unittest.TestCase):
             validator({'name': 'H'})
         self.assertIsInstance(ex.exception.errors[0], voluptuous.LengthInvalid)
 
-    def test_duration(self):
+    def test_017_duration(self):
         schema = {
             voluptuous.Required('duration'): voluptuous.All(voluptuous.Coerce(int), voluptuous.Range(min=1)),
         }
@@ -116,36 +154,34 @@ class DocumentValidationTestCase(unittest.TestCase):
             validator({'duration': None})
 
         doc = validator({'duration': "10"})
-        self.assertEqual(doc, {'duration':10})
+        self.assertEqual(doc, {'duration': 10})
 
         with self.assertRaises(voluptuous.MultipleInvalid):
             doc = validator({'duration': -1})
     
 
-
 class CalculateProposalSchemaTestCase(unittest.TestCase):
     def setUp(self):
         self.validator = voluptuous.Schema(validation_message.CALCULATE_PROPOSAL_SCHEMA)
 
-    def test_01_minimal_schema(self):
-        document = {
-            'agent_official_number_fsma': "fsma",
-            'agreement_date': date_new(2015, 3, 1),
-            'duration': 10,
-            'from_date': date_new(2015, 3, 2),
-            'insured_party__1__birthdate': date_new(1980, 9, 15),
-            'insured_party__1__sex': 'M',
-            'package_id': 10,
-            'premium_schedule__1__coverage_level_type': 'fixed_amount',
-            'premium_schedule__1__premium_fee_1': "10.0",
-            'premium_schedule__1__product_id': 10,
-            'premium_schedule__2__coverage_level_type': None,
-            'premium_schedule__2__product_id': None,
-            'premium_schedules_coverage_limit': "10.0",
-            'premium_schedules_payment_duration': 10,
-            'premium_schedules_period_type': "single",
-            'premium_schedules_premium_rate_1': "10.0",
-        }
-
+    def test_01_calculate_proposal_correct(self):
+        document = load_demo_json('calculate_proposal')
         self.validator(document)
 
+
+class CreateAgreementCodeSchemaTestCase(unittest.TestCase):
+    def setUp(self):
+        self.validator = voluptuous.Schema(validation_message.CREATE_AGREEMENT_CODE_SCHEMA)
+
+    def test_01_create_agreement_code_correct(self):
+        document = load_demo_json('create_agreement_code')
+        self.validator(document)
+
+
+class SendAgreementTestCase(unittest.TestCase):
+    def setUp(self):
+        self.validator = voluptuous.Schema(validation_message.SEND_AGREEMENT_SCHEMA)
+
+    def test_01_send_agreement_correct(self):
+        document = load_demo_json('send_agreement')
+        self.validator(document)
--- src/vfinance_ws/ws/validation_message.py
+++ src/vfinance_ws/ws/validation_message.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python
-import json
 from decimal import Decimal
 import datetime
-from functools import wraps
 
-import voluptuous
 from voluptuous import (
     All,
     Any,
     Coerce,
+    In,
+    Invalid,
     Length,
     Optional,
+    Range,
     Required,
+    Schema,
+    Upper,
+    MultipleInvalid,
+    RequiredFieldInvalid,
 )
-
-
-def IsNone(v):
-    if v is not None:
-        raise ValueError
-    return v
 
 
 def lookup(dic, key, *keys):
@@ -27,33 +25,84 @@ def lookup(dic, key, *keys):
     return dic.get(key)
 
 
-def check_date_field(dic, *keys):
-    date = lookup(dic, *keys)
+def ValidateDate(v):
     try:
-        datetime.date(date['year'], date['month'], date['day'])
-    except ValueError, ex:
-        if 'month must be' in ex.message:
-            path = list(keys) + ['month']
-            raise voluptuous.Invalid(ex.message, path=path)
-        elif 'day is out of' in ex.message:
-            path = list(keys) + ['day']
-            raise voluptuous.Invalid(ex.message, path=path)
+        datetime.date(month=v['month'], year=v['year'], day=v['day'])
+    except ValueError, error:
+        if 'month must be' in error.message:
+            raise Invalid(error.message, path=['month'])
+        elif 'day is out of' in error.message:
+            raise Invalid(error.message, path=['day'])
+    return v
 
 
-def validate_document(document, schema, fields_date=None):
-    validator = voluptuous.Schema(schema)  # , extra=voluptuous.ALLOW_EXTRA)
-    if fields_date is None:
-        fields_date = []
+DATE_SCHEMA = {
+    Required("month"): Range(min=1, max=12),
+    Required("year"): Range(min=1900, max=2400),
+    Required("day"): Range(min=1, max=31),
+}
 
-    errors = {}
+String = lambda **kwargs: All(basestring, Length(**kwargs))
+Date = All(DATE_SCHEMA, ValidateDate)
 
-    validated_document = None
-    # First validation with Voluptuous
+CALCULATE_PROPOSAL_SCHEMA = {
+    Required("agent_official_number_fsma"): String(max=128),
+    Required("agreement_date"): Date,
+    Required("duration"): int,
+    Required("from_date"): Date,
+    Required("insured_party__1__birthdate"): Date,
+    Required("insured_party__1__sex"): All(Upper, In(['M', 'F'])),
+    Required("package_id"): int,
+    Required("premium_schedule__1__premium_fee_1"): Coerce(Decimal),
+    Required("premium_schedule__1__product_id"): int,
+    Required("premium_schedule__2__product_id"): Any(None, int),
+    Required("premium_schedule__1__coverage_level_type"): In(["fixed_amount", "decreasing_amount"]),
+    Required("premium_schedule__2__coverage_level_type"): Any(None, 'decreasing_amount'),
+    Required("premium_schedules_coverage_limit"): Coerce(Decimal),
+    Required("premium_schedules_payment_duration"): int,
+    Required("premium_schedules_period_type"): In(["single", "yearly"]),
+    Required("premium_schedules_premium_rate_1"): Coerce(Decimal),
+}
+
+CREATE_AGREEMENT_CODE_SCHEMA = dict(CALCULATE_PROPOSAL_SCHEMA)
+CREATE_AGREEMENT_CODE_SCHEMA.update({
+    Required("origin"): Length(max=32),
+    Optional('insured_party__1__last_name'): String(max=30),
+    Optional('insured_party__1__first_name'): String(max=30),
+    Optional('insured_party__1__language'): String(max=5),
+    Optional('insured_party__1__nationality_code'): String(max=2),
+    Optional('insured_party__1__social_security_number'): Any(None, Length(max=12)),
+    Optional('insured_party__1__passport_number'): Any(None, Length(max=20)),
+    Optional('insured_party__1__dangerous_hobby'): Any(None, Length(max=20)),
+    Optional('insured_party__1__dangerous_profession'): Any(None, Length(max=20)),
+    Optional('insured_party__1__street_1'): String(max=128),
+    Optional('insured_party__1__city_code'): String(max=10),
+    Optional('insured_party__1__city_name'): String(max=40),
+    Optional('insured_party__1__country_code'): String(max=2),
+    Optional('pledgee_name'): Any(None, Length(max=30)),
+    Optional('pledgee_tax_id'): Any(None, Length(max=20)),
+    Optional('pledgee_reference'): Any(None, Length(max=30)),
+})
+
+SEND_AGREEMENT_SCHEMA = dict(CREATE_AGREEMENT_CODE_SCHEMA)
+SEND_AGREEMENT_SCHEMA.update({
+    Required("signature"): String(max=64),
+    Required("premium_schedule__1__amount"): Coerce(Decimal),
+    Required("premium_schedule__2__amount"): Any(None, Coerce(Decimal)),
+    Required("code"): String(max=32),
+})
+
+
+def validate_document(document, schema):
+    validator = Schema(schema)
+
+    doc, errors = None, {}
+
     try:
-        validated_document = validator(document)
-    except voluptuous.MultipleInvalid as ex:
+        doc = validator(document)
+    except MultipleInvalid as ex:
         for error in ex.errors:
-            if isinstance(error, voluptuous.RequiredFieldInvalid):
+            if isinstance(error, RequiredFieldInvalid):
                 errors[error.path[0].schema] = {
                     u'message': 'Required',
                 }
@@ -64,179 +113,17 @@ def validate_document(document, schema, fields_date=None):
                     u'message': error.error_message,
                     u'value': lookup(document, *error.path)
                 }
+    return doc, errors
 
-    # Second validation for the specific date field (year, month, day)
-    if validated_document:
-        for date_field in fields_date:
-            try:
-                check_date_field(validated_document, *date_field)
-            except voluptuous.Invalid as error:
-                path_str = '/'.join(error.path)
-                errors[path_str] = {
-                    u'message': error.error_message,
-                    u'value': lookup(validated_document, *error.path)
-                }
-    return validated_document, errors
-
-
-DATE_SCHEMA = {
-    Required("month"): voluptuous.Range(min=1, max=12),
-    Required("year"): voluptuous.Range(min=1900, max=2400),
-    Required("day"): voluptuous.Range(min=1, max=31),
-}
-
-
-CALCULATE_PROPOSAL_SCHEMA = {
-    Required("agent_official_number_fsma"): All(basestring, Length(max=128)),
-    Required("agreement_date"): DATE_SCHEMA,
-    Required("duration"): int,
-    # voluptuous.Range(min=1, max=10),
-    Required("from_date"): DATE_SCHEMA,
-    Required("insured_party__1__birthdate"): DATE_SCHEMA,
-    Required("insured_party__1__sex"): voluptuous.In(["M", "F", 'm', 'f']),
-    Required("package_id"): int,
-    Required("premium_schedule__1__premium_fee_1"): Coerce(Decimal),
-    Required("premium_schedule__1__product_id"): int,
-    Required("premium_schedule__2__product_id"): voluptuous.Any(None, int),
-    Required("premium_schedule__1__coverage_level_type"):
-        voluptuous.In(["fixed_amount", "decreasing_amount"]),
-    Required("premium_schedule__2__coverage_level_type"):
-        voluptuous.Any(None, 'decreasing_amount'),
-    Required("premium_schedules_coverage_limit"): Coerce(Decimal),
-    Required("premium_schedules_payment_duration"): int,
-    Required("premium_schedules_period_type"):
-        voluptuous.In(["single", "yearly"]),
-    Required("premium_schedules_premium_rate_1"): Coerce(Decimal),
-}
-
-CREATE_AGREEMENT_CODE_SCHEMA = dict(CALCULATE_PROPOSAL_SCHEMA)
-CREATE_AGREEMENT_CODE_SCHEMA.update({
-    Required("origin"): Length(max=32),
-    Optional('insured_party__1__last_name'): All(basestring, Length(max=30)),
-    Optional('insured_party__1__first_name'): All(basestring, Length(max=30)),
-    Optional('insured_party__1__language'): All(basestring, Length(max=5)),
-    Optional('insured_party__1__nationality_code'): All(basestring, Length(max=2)),
-    Optional('insured_party__1__social_security_number'): Any(None, Length(max=12)),
-    Optional('insured_party__1__passport_number'): Any(None, Length(max=20)),
-    Optional('insured_party__1__dangerous_hobby'): Any(None, Length(max=20)),
-    Optional('insured_party__1__dangerous_profession'): Any(None, Length(max=20)),
-    Optional('insured_party__1__street_1'): All(basestring, Length(max=128)),
-    Optional('insured_party__1__city_code'): All(basestring, Length(max=10)),
-    Optional('insured_party__1__city_name'): All(basestring, Length(max=40)),
-    Optional('insured_party__1__country_code'): All(basestring, Length(max=2)),
-    Optional('pledgee_name'): Any(None, Length(max=30)),
-    Optional('pledgee_tax_id'): Any(None, Length(max=20)),
-    Optional('pledgee_reference'): Any(None, Length(max=30)),
-})
-
-SEND_AGREEMENT_SCHEMA = dict(CREATE_AGREEMENT_CODE_SCHEMA)
-SEND_AGREEMENT_SCHEMA.update({
-    Required("signature"): All(basestring, Length(max=64)),
-    Required("premium_schedule__1__amount"): Coerce(Decimal),
-    Required("premium_schedule__2__amount"): Any(None, Coerce(Decimal)),
-    Required("code"): All(basestring, Length(max=32)),
-})
 
 
 def validation_calculate_proposal(document):
-    FIELDS_DATE = (
-        ['agreement_date'],
-        ['from_date'],
-        ['insured_party__1__birthdate'],
-    )
-
-    return validate_document(
-        document,
-        CALCULATE_PROPOSAL_SCHEMA,
-        FIELDS_DATE
-    )
+    return validate_document(document, CALCULATE_PROPOSAL_SCHEMA)
 
 
 def validation_create_agreement_code(document):
-    FIELDS_DATE = (
-        ['agreement_date'],
-        ['from_date'],
-        ['insured_party__1__birthdate'],
-    )
-
-    return validate_document(
-        document,
-        CREATE_AGREEMENT_CODE_SCHEMA,
-        FIELDS_DATE
-    )
+    return validate_document(document, CREATE_AGREEMENT_CODE_SCHEMA)
 
 
 def validation_send_agreement(document):
-    FIELDS_DATE = (
-        ['agreement_date'],
-        ['from_date'],
-        ['insured_party__1__birthdate'],
-    )
-
-    return validate_document(
-        document,
-        SEND_AGREEMENT_SCHEMA,
-        FIELDS_DATE
-    )
-
-
-if __name__ == '__main__':
-
-    document = """{
-        "insured_party__1__last_name": "FALK",
-        "insured_party__1__first_name": "JULES",
-        "insured_party__1__birthdate": {
-                        "year": 1980,
-                        "month": 2,
-                        "day": 7
-        },
-        "insured_party__1__sex": "M",
-        "insured_party__1__language": "fr_BE",
-        "insured_party__1__nationality_code": "BE",
-        "insured_party__1__social_security_number": "80020705175",
-        "insured_party__1__passport_number": null,
-        "insured_party__1__dangerous_hobby": null,
-        "insured_party__1__dangerous_profession": null,
-        "insured_party__1__street_1": "HAUTE-HEZ 76",
-        "insured_party__1__city_code": {
-                        "year": 4000,
-                        "month": 1,
-                        "day": 1
-        },
-        "insured_party__1__city_name": "GLAIN",
-        "insured_party__1__country_code": "BE",
-        "premium_schedules_coverage_limit": "5602.00",
-        "premium_schedules_payment_duration": 42,
-        "duration": 42,
-        "from_date": {
-                        "year": 2015,
-                        "month": 2,
-                        "day": 26
-        },
-        "premium_schedule__2__product_id": 68,
-        "premium_schedules_coverage_level_type": "decreasing_amount",
-        "premium_schedules_premium_rate_1": "40.00",
-        "premium_schedule__1__premium_fee_1": "50.00",
-        "pledgee_name": "DHB BANK",
-        "pledgee_tax_id": "BE 0464.655.437",
-        "pledgee_reference": "350130145",
-        "premium_schedules_period_type": "single",
-        "package_id": 64,
-        "premium_schedule__1__product_id": 67,
-        "agreement_date": {
-                        "year": 2015,
-                        "month": 2,
-                        "day": 26
-        },
-        "agent_official_number_fsma": "12345",
-        "origin": "BIA:1234"
-        }
-    """
-    document = json.loads(document)
-    # with open('calculate_proposal.json') as jsonfile:
-    #     document = json.load(jsonfile)
-    #     validated_document, errors = validation_calculate_proposal(document)
-    validator = voluptuous.Schema(CREATE_AGREEMENT_CODE_SCHEMA)  # , extra=voluptuous.ALLOW_EXTRA)
-    validated_document = validator(document)
-
-
+    return validate_document(document, SEND_AGREEMENT_SCHEMA)
