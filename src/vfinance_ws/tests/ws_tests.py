f82e83bf7dfc8b75d1935aa662de1c6fa0f6dbb9
Stephane Wirtel <stephane@wirtel.be>
2015-03-05 12:47:27 +0100

    [REF] Use the demo/*.json files for the unit tests

--- src/vfinance_ws/tests/ws_tests.py
+++ src/vfinance_ws/tests/ws_tests.py
@@ -10,6 +10,15 @@ try:
 except ImportError:
     set_trace = lambda: None
 
+import os
+DEMO_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'demo')
+print DEMO_DIR
+
+def load_demo_json(fname):
+    with open(os.path.join(DEMO_DIR, "%s.json" % (fname,))) as infile:
+        return json.load(infile)
+
+
 # @unittest.skip("Skip")
 class WsTestCase(unittest.TestCase):
     def setUp(self):
@@ -31,44 +40,6 @@ class WsTestCase(unittest.TestCase):
         if isinstance(data, dict):
             data = json.dumps(data)
         return self.client.post(url, headers=headers, data=data)
-
-    def create_calculate_proposal_document(self, **kwargs):
-        document = {
-            "agent_official_number_fsma": "Agent Official Number FSMA",
-            "agreement_date": {
-                "month": 3,
-                "year": 2015,
-                "day": 2
-            },
-            "duration": 10,
-            "from_date": {
-                "month": 3,
-                "year": 2015,
-                "day": 1
-            },
-            "insured_party__1__birthdate": {
-                "month": 9,
-                "year": 1980,
-                "day": 15
-            },
-            "insured_party__1__sex": "M",
-            "package_id": 64,
-            "premium_schedule__1__premium_fee_1": "2.00",
-            "premium_schedule__1__product_id": 67,
-            "premium_schedule__2__product_id": None,
-            "premium_schedule__1__coverage_level_type": "fixed_amount",
-            "premium_schedule__2__coverage_level_type": None,
-            "premium_schedules_coverage_limit": "5000",
-            "premium_schedules_payment_duration": 10,
-            "premium_schedules_period_type": "single",
-            "premium_schedules_premium_rate_1": "20"
-        }
-        document.update(**kwargs)
-        return document
-
-    def create_agreement_code_document(self, **kwargs):
-        return self.create_calculate_proposal_document(origin='BIA:10', **kwargs)
-
 
     # @unittest.skip("")
     def test_001_calculate_proposal_bad_content_type(self):
@@ -105,12 +76,13 @@ class WsTestCase(unittest.TestCase):
 
     # @unittest.skip("")
     def test_010_calculate_proposal(self):
-        document = self.create_calculate_proposal_document()
+        document = load_demo_json('calculate_proposal')
         response = self.post_json('calculate_proposal', data=document)
+        # set_trace()
         self.assertEqual(response.status_code, 200)
 
     def test_011_calculate_proposal_two_products(self):
-        document = self.create_calculate_proposal_document()
+        document = load_demo_json('calculate_proposal')
         document['premium_schedule__2__product_id'] = 68
         document['premium_schedule__2__coverage_level_type'] = 'decreasing_amount'
         response = self.post_json('calculate_proposal', data=document)
@@ -118,8 +90,7 @@ class WsTestCase(unittest.TestCase):
 
     # @unittest.skip("")
     def test_012_calculate_proposal_missing_fields(self):
-        DOCUMENT = {}
-        response = self.post_json('calculate_proposal', data=DOCUMENT)
+        response = self.post_json('calculate_proposal', data={})
 
         message = json.loads(response.data)
 
@@ -130,7 +101,7 @@ class WsTestCase(unittest.TestCase):
 
     # @unittest.skip("")
     def test_013_calculate_proposal_bad_values(self):
-        document = self.create_calculate_proposal_document()
+        document = load_demo_json('calculate_proposal')
         document['agreement_date']['month'] = 2
         document['agreement_date']['day'] = 29
 
@@ -142,14 +113,13 @@ class WsTestCase(unittest.TestCase):
 
     # @unittest.skip("")
     def test_020_create_agreement_code(self):
-        document = self.create_agreement_code_document()
-
+        document = load_demo_json('create_agreement_code')
         response = self.post_json('create_agreement_code', data=document)
         self.assertEqual(response.status_code, 200)
 
     # @unittest.skip("")
     def test_021_create_agreement_code_wrong_values(self):
-        document = self.create_agreement_code_document()
+        document = load_demo_json('create_agreement_code')
         document.update({
             'insured_party__1__nationality_code': 'qwertyuio'
         })
@@ -185,13 +155,7 @@ class WsTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 501)
 
     def test_080_send_agreement(self):
-        document = self.create_agreement_code_document()
-        document.update({
-            'code': '000/0000/00000',
-            'premium_schedule__1__amount': '10.0',
-            'premium_schedule__2__amount': None,
-            'signature': 'Signature'
-        })
+        document = load_demo_json('send_agreement')
         response = self.post_json('send_agreement', data=document)
         self.assertEqual(response.status_code, 200)
         content = json.loads(response.data)
