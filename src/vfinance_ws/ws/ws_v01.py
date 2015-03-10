1302d274a821303a837a73c07048c1be8dd9ccab
Stephane Wirtel <stephane@wirtel.be>
2015-03-10 17:43:13 +0100

    [IMP] Simplify the code with new decorators

--- src/vfinance_ws/ws/ws_v01.py
+++ src/vfinance_ws/ws/ws_v01.py
@@ -1,23 +1,90 @@
-from __future__ import absolute_import
+import functools
+
+import werkzeug.exceptions
 
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 
 import camelot.core.exception
 import camelot.core.utils
 
-from vfinance_ws.ws.decorators import log_to_file, check_minimal_requirements
-from vfinance_ws.ws.utils import is_json_body
+import voluptuous
+
+from vfinance_ws.ws.decorators import log_to_file
+
+from vfinance_ws.ws.validation_message import (
+    validation_calculate_proposal,
+    validation_create_agreement_code,
+    validation_send_agreement,
+)
+from vfinance_ws.ws.exceptions import BadContentType
 
 from vfinance_ws.api import v01
 
 bp = Blueprint('api_v01', __name__)
 
+def ws_jsonify(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            result = function(*args, **kwargs)
+
+            if result is None:
+                result = {}
+
+            return jsonify(result)
+        except voluptuous.MultipleInvalid as ex:
+            errors = {}
+            for error in ex.errors:
+                if isinstance(error, voluptuous.RequiredFieldInvalid):
+                    errors[error.path[0].schema] = {
+                        u'message': 'Required',
+                    }
+                else:
+                    path_str = '/'.join(error.path)
+                    errors[path_str] = {
+                        u'message': error.error_message
+                    }
+            return jsonify(errors), 400
+        except BadContentType, ex:
+            raise
+        except Exception as ex:
+            msg = ex.message
+            if isinstance(ex, camelot.core.exception.UserException):
+                if isinstance(ex.message, camelot.core.utils.ugettext_lazy):
+                    msg = ex.message._string_to_translate
+            return jsonify({'message': msg}), 400
+
+    return wrapper
+
+def validation_json(validator=None):
+    def decorator(function):
+        @functools.wraps(function)
+        def wrapper(*args, **kwargs):
+            if not request.content_type:
+                raise BadContentType('Content-Type is not setted')
+
+            if 'application/json' not in request.content_type:
+                raise BadContentType("Content-Type is not 'application/json'")
+
+            if validator is None:
+                return function(*args, **kwargs)
+            else:
+                try:
+                    document = validator(request.get_json())
+                    return function(document, *args, **kwargs)
+                except werkzeug.exceptions.BadRequest, ex:
+                    raise BadContentType('Invalid JSON message')
+
+        return wrapper
+    return decorator
+
 
 @bp.route('/calculate_proposal', methods=['POST'])
-@log_to_file
-@check_minimal_requirements
-def calculate_proposal():
+@ws_jsonify
+@validation_json(validation_calculate_proposal)
+def calculate_proposal(document):
     """
     :synopsis: Calculate the amount of a proposal
     :reqheader Content-Type: :mimetype:`application/json`
@@ -64,30 +131,14 @@ def calculate_proposal():
         :language: json
 
     """
-    json_document = is_json_body()
-
-    # Check the json
-
-    from .validation_message import validation_calculate_proposal
-
-    proposal, errors = validation_calculate_proposal(json_document)
-
-    if errors:
-        return jsonify(errors), 400
-
-    try:
-        return jsonify(v01.calculate_proposal(proposal))
-    except Exception, ex:
-        if isinstance(ex, camelot.core.exception.UserException):
-            if isinstance(ex.message, camelot.core.utils.ugettext_lazy):
-                return jsonify({'message': ex.message._string_to_translate}), 400
-        return jsonify({'message': ex.message}), 400
+    return v01.calculate_proposal(document)
 
 
 @bp.route('/create_agreement_code', methods=['POST'])
+@ws_jsonify
 @log_to_file
-@check_minimal_requirements
-def create_agreement_code():
+@validation_json(validation_create_agreement_code)
+def create_agreement_code(document):
     """
     :synopsis: Create an Agreement Code
 
@@ -110,29 +161,12 @@ def create_agreement_code():
     :resheader Cotnent-Type: :mimetype:`application/json`
 
     """
-    json_document = is_json_body()
-
-    # Check the json
-
-    from .validation_message import validation_create_agreement_code
-
-    proposal, errors = validation_create_agreement_code(json_document)
-
-    if errors:
-        return jsonify(errors), 400
-
-    try:
-        return jsonify(v01.create_agreement_code(proposal))
-    except Exception, ex:
-        if isinstance(ex, camelot.core.exception.UserException):
-            if isinstance(ex.message, camelot.core.utils.ugettext_lazy):
-                return jsonify({'message': ex.message._string_to_translate}), 400
-        return jsonify({'message': ex.message}), 400
-
+    return v01.create_agreement_code(document)
 
 @bp.route('/send_agreement', methods=['POST'])
-@check_minimal_requirements
-def send_agreement():
+@ws_jsonify
+@validation_json(validation_send_agreement)
+def send_agreement(document):
     """
     :synopsis: Send an agreement
 
@@ -145,22 +179,7 @@ def send_agreement():
     :reqheader Content-Type: :mimetype:`application/json`
     :resheader Content-Type: :mimetype:`application/json`
     """
-    json_document = is_json_body()
-    from .validation_message import validation_send_agreement
-
-    proposal, errors = validation_send_agreement(json_document)
-
-    if errors:
-        return jsonify(errors), 400
-
-    try:
-        v01.send_agreement(proposal)
-        return jsonify({})
-    except Exception, ex:
-        if isinstance(ex, camelot.core.exception.UserException):
-            if isinstance(ex.message, camelot.core.utils.ugettext_lazy):
-                return jsonify({'message': ex.message._string_to_translate}), 400
-        return jsonify({'message': ex.message}), 400 
+    return v01.send_agreement(document)
 
 
 @bp.route('/get_proposal_pdf', methods=['POST'])
