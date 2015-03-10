4d9be91fc30a6e5d0b53fe8aaccea10197163748
Erik Janssens <erik.janssens@vortex-financials.be>
2015-03-10 10:03:58 +0100

    use cdecimal if available

---
+++ src/vfinance_ws/__init__.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+LOGGER = logging.getLogger( 'vfinance_ws' )
+
+try:
+    import cdecimal
+    sys.modules["decimal"] = cdecimal
+    LOGGER.info('using cdecimal instead of decimal')
+except:
+    LOGGER.info('using decimal, cdecimal import failed')
