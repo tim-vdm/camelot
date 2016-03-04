f1463c0d957c5c2126b753c54d3a977a13e16ff2
Pieter-Jan Delaruelle <pieter-jan.delaruelle@vortex-financials.be>
2016-03-04 09:25:35 +0100

    Should run from same sdk as ws, then cloudlaunch should be available

--- src/fabfile.py
+++ src/fabfile.py
@@ -68,7 +68,7 @@ def build_upload():
 
 def run_local():
     with context_managers.lcd('dist/cloud'), context_managers.shell_env(LOGHOME='/tmp/log-vfws.txt'), context_managers.shell_env(DB_PATH='/tmp/test.db'):
-        api.local('python -m cloudlaunch2.main'
+        api.local('python -m cloudlaunch.main'
                   ' --cld-file=v-finance-web-service-{0.CONFIGURATION}.cld'
                   ' --cld-name=V-Finance-WS'
                   ' --cld-branch={0.CONFIGURATION}'
